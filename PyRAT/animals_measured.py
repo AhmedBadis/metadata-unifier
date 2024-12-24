334f632adb2fe5b4e4051cfc15155167982ada85
AhmedBadis <ahmedbadislakrach@gmail.com>
2024-12-24 12:24:54 +0100

    ADD: separate files for measurements

--- PyRAT/animals_measured.py
+++ PyRAT/animals_measured.py
@@ -1,6 +1,8 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from pprint import pprint
+import psutil
+import os
 
 # Login credentials
 base_url = 'https://pyrat.ukbonn.de/pyrat-test/api/v3'
@@ -75,81 +77,81 @@ params = {
     ],
     's': "species_name:asc",  # Example sort parameter
 
-    # Unused sort parameters (Max 1)
-    # "eartag_or_id:asc",
-    # "eartag_or_id:desc",
-    # "prefix:asc",
-    # "prefix:desc",
-    # "labid:asc",
-    # "labid:desc",
-    # "cohort_id:asc",
-    # "cohort_id:desc",
-    # "origin_name:asc",
-    # "origin_name:desc",
-    # "sex:asc",
-    # "sex:desc",
-    # "species_name:asc",
-    # "species_name:desc",
-    # "datesacrificed:asc",
-    # "datesacrificed:desc",
-    # "cagenumber:asc",
-    # "cagenumber:desc",
-    # "cagelabel:asc",
-    # "cagelabel:desc",
-    # "cage_owner_username:asc",
-    # "cage_owner_username:desc",
-    # "cage_owner_fullname:asc",
-    # "cage_owner_fullname:desc",
-    # "rack_description:asc",
-    # "rack_description:desc",
-    # "room_name:asc",
-    # "room_name:desc",
-    # "area_name:asc",
-    # "area_name:desc",
-    # "building_name:asc",
-    # "building_name:desc",
-    # "responsible_fullname:asc",
-    # "responsible_fullname:desc",
-    # "owner_username:asc",
-    # "owner_username:desc",
-    # "owner_fullname:asc",
-    # "owner_fullname:desc",
-    # "age_days:asc",
-    # "age_days:desc",
-    # "age_weeks:asc",
-    # "age_weeks:desc",
-    # "dateborn:asc",
-    # "dateborn:desc",
-    # "date_last_comment:asc",
-    # "date_last_comment:desc",
-    # "strain_name:asc",
-    # "strain_name:desc",
-    # "strain_name_id:asc",
-    # "strain_name_id:desc",
-    # "strain_name_with_id:asc",
-    # "strain_name_with_id:desc",
-    # "licence_title:asc",
-    # "licence_title:desc",
-    # "licence_id:asc",
-    # "licence_id:desc",
-    # "licence_number:asc",
-    # "licence_number:desc",
-    # "classification_id:asc",
-    # "classification_id:desc",
-    # "classification:asc",
-    # "classification:desc",
-    # "pregnant_days:asc",
-    # "pregnant_days:desc",
-    # "plug_date:asc",
-    # "plug_date:desc",
-    # "wean_date:asc",
-    # "wean_date:desc",
-    # "projects:asc",
-    # "projects:desc",
-    # "weight:asc",
-    # "weight:desc",
-    # "import_order_request_id:asc",
-    # "import_order_request_id:desc",
+        # Unused sort parameters (Max 1)
+        # "eartag_or_id:asc",
+        # "eartag_or_id:desc",
+        # "prefix:asc",
+        # "prefix:desc",
+        # "labid:asc",
+        # "labid:desc",
+        # "cohort_id:asc",
+        # "cohort_id:desc",
+        # "origin_name:asc",
+        # "origin_name:desc",
+        # "sex:asc",
+        # "sex:desc",
+        # "species_name:asc",
+        # "species_name:desc",
+        # "datesacrificed:asc",
+        # "datesacrificed:desc",
+        # "cagenumber:asc",
+        # "cagenumber:desc",
+        # "cagelabel:asc",
+        # "cagelabel:desc",
+        # "cage_owner_username:asc",
+        # "cage_owner_username:desc",
+        # "cage_owner_fullname:asc",
+        # "cage_owner_fullname:desc",
+        # "rack_description:asc",
+        # "rack_description:desc",
+        # "room_name:asc",
+        # "room_name:desc",
+        # "area_name:asc",
+        # "area_name:desc",
+        # "building_name:asc",
+        # "building_name:desc",
+        # "responsible_fullname:asc",
+        # "responsible_fullname:desc",
+        # "owner_username:asc",
+        # "owner_username:desc",
+        # "owner_fullname:asc",
+        # "owner_fullname:desc",
+        # "age_days:asc",
+        # "age_days:desc",
+        # "age_weeks:asc",
+        # "age_weeks:desc",
+        # "dateborn:asc",
+        # "dateborn:desc",
+        # "date_last_comment:asc",
+        # "date_last_comment:desc",
+        # "strain_name:asc",
+        # "strain_name:desc",
+        # "strain_name_id:asc",
+        # "strain_name_id:desc",
+        # "strain_name_with_id:asc",
+        # "strain_name_with_id:desc",
+        # "licence_title:asc",
+        # "licence_title:desc",
+        # "licence_id:asc",
+        # "licence_id:desc",
+        # "licence_number:asc",
+        # "licence_number:desc",
+        # "classification_id:asc",
+        # "classification_id:desc",
+        # "classification:asc",
+        # "classification:desc",
+        # "pregnant_days:asc",
+        # "pregnant_days:desc",
+        # "plug_date:asc",
+        # "plug_date:desc",
+        # "wean_date:asc",
+        # "wean_date:desc",
+        # "projects:asc",
+        # "projects:desc",
+        # "weight:asc",
+        # "weight:desc",
+        # "import_order_request_id:asc",
+        # "import_order_request_id:desc",
     'o': 0,  # Number of items to skip from the beginning
     'l': 100,  # Maximum amount of returned items
     'status': "active"  # Animal status
@@ -158,9 +160,20 @@ params = {
 # Start a session
 session = requests.Session()
 
+# Measure memory usage before and after the API call
+process = psutil.Process(os.getpid())
+memory_before = process.memory_info().rss / (1024 ** 2)  # in MB
 
 # Perform login and request data with query parameters
 response = session.get(base_url + '/animals', auth=payload, params=params)
+
+memory_after = process.memory_info().rss / (1024 ** 2)  # in MB
+
+print(f"Memory Usage Before: {memory_before} MB")
+print(f"Memory Usage After: {memory_after} MB")
+print(f"Memory Difference: {memory_after - memory_before} MB")
+response_time = response.elapsed.total_seconds()
+print(f"Response Time: {response_time} seconds")
 
 # Check for successful response
 if response.status_code == 200:
